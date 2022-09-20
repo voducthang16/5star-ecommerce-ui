e3919ca4411fb76413785210ebf983283ddb1d13
Tran Phat <phat.trantan@h2-inc.com>
2022-09-20 14:11:54 +0700

    loading gif comp code

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import images from '~/assets/images';
 const Loading = () => {
     return (
-        <div className="loading">
-            <img src={images.LoadingGif} alt="" />
+        <div className="loading fixed bg-white inset-0 z-[99]">
+            <img
+                src={images.LoadingGif}
+                alt=""
+                className="w-[100px] absolute top-[50%] left-[50%] -translate-y-2/4 -translate-x-2/4"
+            />
         </div>
     );
 };
