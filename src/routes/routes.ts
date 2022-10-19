8d6c356869fa398d63a50d2eb1c5ca154ba7956e
GB <voducthang16@gmail.com>
2022-10-19 12:38:06 +0700

    fix bug

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -6,7 +6,7 @@ import Home from '~/pages/Home';
 import Login from '~/pages/Login';
 
 import ProductDetail from '~/pages/ProductDetail';
-import Category from '~/pages/Category';
+import Category from '~/pages/Categories';
 import Register from '~/pages/Register';
 
 interface Routes {
