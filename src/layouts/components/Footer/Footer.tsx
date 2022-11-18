eee2d79e55dd2d45ffc4d3799c2a51fd5c2ff4a6
GB <voducthang16@gmail.com>
2022-11-18 20:13:49 +0700

    fix UI
    18.11.2022 - 8:10 PM

--- src/layouts/components/Footer/Footer.tsx
+++ src/layouts/components/Footer/Footer.tsx
@@ -21,69 +21,32 @@ function Footer() {
                 <div className="container">
                     <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                         <div className="col-span-1">
-                            <h4 className="text-lg font-bold mb-8">Loại</h4>
+                            <h4 className="text-lg font-bold mb-8">Về chúng tôi</h4>
                             <ul className="list-menu text-base font-normal space-y-4">
                                 <li>
-                                    <a href="/">Giày thể thao</a>
+                                    <a href="/">Giới thiệu công ty</a>
                                 </li>
                                 <li>
-                                    <a href="/">Trang phục thể thao</a>
+                                    <a href="/">Đội ngũ</a>
                                 </li>
                                 <li>
-                                    <a href="/">Sandals</a>
-                                </li>
-                                <li>
-                                    <a href="/">Quần jean</a>
-                                </li>
-                                <li>
-                                    <a href="/">Quần short</a>
-                                </li>
-                                <li>
-                                    <a href="/">Áo phông</a>
-                                </li>
-                                <li>
-                                    <a href="/">Đồ bơi</a>
-                                </li>
-                                <li>
-                                    <a href="/">Phụ kiện</a>
-                                </li>
-                                <li>
-                                    <a href="/">Đồng hồ</a>
+                                    <a href="/">Tin tức</a>
                                 </li>
                             </ul>
                         </div>
-                        <div className="col-span-1 space-y-8">
-                            <div>
-                                <h4 className="text-lg font-bold mb-8">Khám phá</h4>
-                                <ul className="list-menu text-base font-normal space-y-4">
-                                    <li>
-                                        <a href="/">Hệ thống thiết kế</a>
-                                    </li>
-                                    <li>
-                                        <a href="/">Bảng giá</a>
-                                    </li>
-                                    <li>
-                                        <a href="/">Bảo mật</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div>
-                                <h4 className="text-lg font-bold mb-8">Về chúng tôi</h4>
-                                <ul className="list-menu text-base font-normal space-y-4">
-                                    <li>
-                                        <a href="/">Giới thiệu công ty</a>
-                                    </li>
-                                    <li>
-                                        <a href="/">Đội ngũ</a>
-                                    </li>
-                                    <li>
-                                        <a href="/">Nghề nghiệp</a>
-                                    </li>
-                                    <li>
-                                        <a href="/">Tin tức</a>
-                                    </li>
-                                </ul>
-                            </div>
+                        <div className="col-span-1">
+                            <h4 className="text-lg font-bold mb-8">Khám phá</h4>
+                            <ul className="list-menu text-base font-normal space-y-4">
+                                <li>
+                                    <a href="/">Hệ thống thiết kế</a>
+                                </li>
+                                <li>
+                                    <a href="/">Bảng giá</a>
+                                </li>
+                                <li>
+                                    <a href="/">Bảo mật</a>
+                                </li>
+                            </ul>
                         </div>
                         <div className="col-span-1">
                             <h4 className="text-lg font-bold mb-8">Hãy liên lạc với chúng tôi</h4>
@@ -106,13 +69,13 @@ function Footer() {
                                 phẩm mới thông tin.
                             </p>
                             <div className="relative flex md:flex-col md:items-start lg:items-center lg:flex-row">
-                                <form className="w-full">
+                                <form className="w-full lg:flex lg:items-center">
                                     <input
                                         className="text-base p-3 bg-[#272938] outline-none text-white w-[70%] mr-1 rounded-md"
                                         type="text"
                                         placeholder="Nhập email của bạn"
                                     />
-                                    <Button colorScheme="teal" className="!h-[48px]" rightIcon={<FiSend />}>
+                                    <Button colorScheme="teal" className="!h-12 flex-1" rightIcon={<FiSend />}>
                                         Gửi
                                     </Button>
                                 </form>
@@ -170,10 +133,10 @@ function Footer() {
                         </div>
                     </div>
                     <div className="col-span-2 md:col-span-4 border-t border-[#3a3d53] pt-10">
-                        <div className="flex flex-col md:flex-row justify-between space-y-4">
-                            <div className="w-full">
-                                <div className="logo w-[40%]">
-                                    <Logo />
+                        <div className="flex flex-col md:flex-row justify-between space-y-4 lg:space-y-0">
+                            <div className="flex-1">
+                                <div className="logo w-full lg:w-2/5 lg:h-16">
+                                    <Logo className="mx-auto md:mx-0" />
                                 </div>
                                 <ul className="flex flex-wrap justify-center md:justify-start mt-6 text-sm space-x-4 leading-6">
                                     <li className="cursor-pointer hover:opacity-80 transition-all duration-200">
@@ -192,14 +155,14 @@ function Footer() {
                                         Điều khoản sử dụng
                                     </li>
                                 </ul>
-                                <p className="mt-8 text-center md:text-start text-xs">
+                            </div>
+                            <div className="flex-1 md:w-1/2">
+                                <div className="h-16 flex items-center justify-center md:justify-end">
+                                    <Image src={images.cards} alt="cards" className="w-3/5 object-contain" />
+                                </div>
+                                <p className="mt-6 leading-6 text-center md:text-end text-sm">
                                     © Đã đăng ký Bản quyền. Thực hiện bởi 5Star Team
                                 </p>
-                            </div>
-                            <div className="flex flex-col md:items-end items-center space-y-4">
-                                <div className="w-3/5">
-                                    <Image src={images.cards} alt="cards" />
-                                </div>
                             </div>
                         </div>
                     </div>
